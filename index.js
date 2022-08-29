d08021e941a4956151ce2f43dcc96b5b3008267a
Klyrasel <KaiLyrasel@outlook.com>
2022-08-29 16:17:51 -0400

    callback function working

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ newItem('assets/staff.png', 600, 250)
 const character = newImage('assets/green-character/static.gif')
 
 function handleDirectionChange(direction) {
-if(direction === null){
+if(direction === null)
+{
     character.src = 'assets/green-character/static.gif'
 }
 if(direction === 'west'){
@@ -92,35 +93,6 @@ character.style.bottom = y + 'px'
 
 setInterval (moveCharacter, 1)
 
-function moveWithArrowKeys(left,bottom,callback) {
-    let direction = null;
-    let x = left;
-    let y = bottom;
-
-document.addEventListener('keydown', function(e) {    
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-    callback(direction)
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-    callback(direction)
-})
-
-}
 
 function move(element) {
     element.style.position = 'fixed'
@@ -130,7 +102,7 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom) {
+    function moveWithArrowKeys(left, bottom, callback) {
         let direction = null;
         let x = left;
         let y = bottom;
@@ -172,10 +144,12 @@ function move(element) {
             if(e.key === 'ArrowDown'){
                 direction = 'south'
             }
+            callback(direction)
         })
         
         document.addEventListener('keyup', function(e) {
             direction = null
+            callback(direction)
         })
     }
     
